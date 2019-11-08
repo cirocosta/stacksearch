6526e071763f12800573b04bc73cb2be2c4a963e
Ciro S. Costa <cscosta@pivotal.io>
2019-11-08 11:07:05 -0500

    remove unused boilerplate code
    Signed-off-by: Ciro S. Costa <cscosta@pivotal.io>

--- cmd/stacksearch/stacksearch.go
+++ cmd/stacksearch/stacksearch.go
@@ -1,50 +1,5 @@
 package main
 
-import (
-	"io"
-	"os"
-
-	"github.com/pkg/errors"
-)
-
 var stacksearch struct {
 	Index indexCommand `command:"index"`
-}
-
-func writer(value string) (w io.Writer, err error) {
-	var file *os.File
-
-	if value == "-" {
-		w = os.Stdout
-		return
-	}
-
-	file, err = os.Create(value)
-	if err != nil {
-		err = errors.Wrapf(err,
-			"failed to create file %s", value)
-		return
-	}
-
-	w = file
-	return
-}
-
-func reader(value string) (r io.Reader, err error) {
-	var file *os.File
-
-	if value == "-" {
-		r = os.Stdin
-		return
-	}
-
-	file, err = os.Open(value)
-	if err != nil {
-		err = errors.Wrapf(err,
-			"failed to open dpkg status file at %s", value)
-		return
-	}
-
-	r = file
-	return
 }
